50ecbe34c816cd2ecc0378591badf38855a409ed
Jl115 <joelbern006@gmail.com>
2024-05-20 21:08:34 +0200

    chore: Update npm dependencies and add dotenv configuration for server updated docker-compose for using my own docker image for llm

---
+++ server/config/config.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+module.exports = {
+  development: {
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME || "lb-chat",
+    host: process.env.DB_HOST || "127.0.0.1",
+    port: process.env.DB_PORT || 9091,
+    dialect: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  },
+  test: {
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME || "lb-chat",
+    host: process.env.DB_HOST || "127.0.0.1",
+    port: process.env.DB_PORT || 9091,
+    dialect: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  },
+  production: {
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME || "lb-chat",
+    host: process.env.DB_HOST || "127.0.0.1",
+    port: process.env.DB_PORT || 9091,
+    dialect: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  },
+};
--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,10 @@
-const dotenv = require("dotenv");
 const express = require("express");
 const cors = require("cors");
+const db = require("../models/index.js");
 
 // Set path to .env file
-dotenv.config({ path: "./.env" });
+require("dotenv").config();
+
 const routes = require("./Routes/routes.js");
 
 // Initialize app
@@ -19,6 +20,19 @@ app.use(express.urlencoded({ extended: true }));
 
 //Routes
 app.use("/", routes);
+console.log(
+  "\x1b[33m%s\x1b[0m",
+  " ,process.env.DEV_DB_USERNAME--------------------",
+  process.env.DB_USER
+);
+
+// Test connection for Database
+db.sequelize
+  .authenticate()
+  .then(() =>
+    console.log("Database connection has been established successfully.")
+  )
+  .catch((error) => console.error("Unable to connect to the database:", error));
 
 // Start server
 app.listen(process.env.SERVER_PORT, () => {
